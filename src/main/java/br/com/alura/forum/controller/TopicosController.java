8a120eb2f214072f785c5a696bad15bab1074849
Fernando Santos <fernando.santos@img.com.br>
2019-07-16 13:41:51 -0300

    Implement DELETE operation

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,4 +72,11 @@ public class TopicosController {
 		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		this.topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
+	}
 }
