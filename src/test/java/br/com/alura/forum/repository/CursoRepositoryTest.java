ba64445ab4a4f3785b8b4e47380b8e44ce1103dc
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-08 11:40:11 -0300

    Implement repository test
    - Use @DataJpaTest in order to offer transaction handle
    - Use @Test from jupiter

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.model.Curso;
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloNome() {
+        String nomeCurso = "HTML 5";
+        Curso curso = this.repository.findByNome(nomeCurso);
+        Assertions.assertNotNull(curso);
+        Assertions.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEstaCadastrado() {
+        String nomeCurso = "JPA";
+        Curso curso = this.repository.findByNome(nomeCurso);
+        Assertions.assertNull(curso);
+    }
+}
