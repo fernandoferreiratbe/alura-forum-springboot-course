40e05d5575a9e4030d64e81abb5462fc289dec4c
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-14 13:21:50 -0300

    Add suport to test entity manager
    - Use a empty database to perform the tests through application.properties

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
@@ -17,10 +18,20 @@ public class CursoRepositoryTest {
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloNome() {
         String nomeCurso = "HTML 5";
+        String categoria = "Programação";
+        Curso cursoHtml = new Curso();
+        cursoHtml.setNome(nomeCurso);
+        cursoHtml.setCategoria(categoria);
+
+        this.testEntityManager.persist(cursoHtml);
         Curso curso = this.repository.findByNome(nomeCurso);
+
         Assertions.assertNotNull(curso);
         Assertions.assertEquals(nomeCurso, curso.getNome());
     }
