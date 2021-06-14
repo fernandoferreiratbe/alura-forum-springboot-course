d364af303ae8758bbaabb4c2f25f375cc63e760e
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-14 16:27:00 -0300

    Implement first authentication controller test
    - Use mock mvc to test request and validate bad request

---
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@SpringBootTest
+@ActiveProfiles(value = "test")
+@AutoConfigureMockMvc
+public class AutenticacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetornar400AoReceberLoginIncorreto() throws Exception {
+        URI uri = new URI("/auth");
+        String corpoDaRequisicao = "{ \"email\": \"invalid@me.com\", \"senha\": \"123mudar\" }";
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                    .post(uri)
+                    .content(corpoDaRequisicao)
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                    .status()
+                    .is(400));
+    }
+}
