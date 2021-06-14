83d0412da1024f195728d4646cc40999b1ddd3b0
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-14 11:56:47 -0300

    Set spring to use the database defined
    - That annotation tells spring to use the defined database into
    application.properties instead of looking for a memory database.

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRepositoryTest {
 
     @Autowired
