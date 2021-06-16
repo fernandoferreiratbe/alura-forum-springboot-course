c499c78f0b7becd297702b47962dce1953a4d472
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-15 23:59:32 -0300

    Add support to active profile for ForumApplicationTest
    - Set test as profile for that class instead of default

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -2,9 +2,11 @@ package br.com.alura.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 public class ForumApplicationTests {
 
 	@Test
